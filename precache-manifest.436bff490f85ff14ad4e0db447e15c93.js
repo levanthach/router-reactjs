a39d94aefd7cce150c354cdb73581e565112dbc3
lethach <lethach150699@gmail.com>
2020-03-22 15:30:48 +0700

    Updates

---
+++ precache-manifest.436bff490f85ff14ad4e0db447e15c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61259bdf8561433660bcfa7ecceaa02a",
+    "url": "/router-reactjs/index.html"
+  },
+  {
+    "revision": "ba9c1fe2f137bcc71e15",
+    "url": "/router-reactjs/static/css/main.5ee62e64.chunk.css"
+  },
+  {
+    "revision": "087aa20b636fecad784b",
+    "url": "/router-reactjs/static/js/2.a4f0d25b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/router-reactjs/static/js/2.a4f0d25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9c1fe2f137bcc71e15",
+    "url": "/router-reactjs/static/js/main.63b66c0a.chunk.js"
+  },
+  {
+    "revision": "40c52da85fd8a37ddb57",
+    "url": "/router-reactjs/static/js/runtime-main.79c60f9d.js"
+  }
+]);
